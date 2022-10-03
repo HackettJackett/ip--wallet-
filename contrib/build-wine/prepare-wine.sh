4010fe3b4d16042813935a95a4415a350136a930
HackettJackett <105892924+HackettJackett@users.noreply.github.com>
2022-10-03 02:49:55 -0400

    Initialize sqlite_db

---
+++ contrib/build-wine/prepare-wine.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Please update these carefully, some versions won't work under Wine
+NSIS_FILENAME=nsis-3.04-setup.exe
+NSIS_URL=https://prdownloads.sourceforge.net/nsis/$NSIS_FILENAME?download
+NSIS_SHA256=4e1db5a7400e348b1b46a4a11b6d9557fd84368e4ad3d4bc4c1be636c89638aa
+
+ZBAR_FILENAME=zbarw-20121031-setup.exe
+ZBAR_URL=https://sourceforge.net/projects/zbarw/files/$ZBAR_FILENAME/download
+ZBAR_SHA256=177e32b272fa76528a3af486b74e9cb356707be1c5ace4ed3fcee9723e2c2c02
+
+LIBUSB_FILENAME=libusb-1.0.22.7z
+LIBUSB_URL=https://prdownloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.22/$LIBUSB_FILENAME?download
+LIBUSB_SHA256=671f1a420757b4480e7fadc8313d6fb3cbb75ca00934c417c1efa6e77fb8779b
+
+PYTHON_VERSION=3.6.8
+
+## These settings probably don't need change
+export WINEPREFIX=/opt/wine64
+#export WINEARCH='win32'
+
+PYTHON_FOLDER="python3"
+PYHOME="c:/$PYTHON_FOLDER"
+PYTHON="wine $PYHOME/python.exe -OO -B"
+
+
+# Let's begin!
+here="$(dirname "$(readlink -e "$0")")"
+set -e
+
+. $here/../build_tools_util.sh
+
+wine 'wineboot'
+
+
+cd /tmp/electrum-build
+
+# Install Python
+# note: you might need "sudo apt-get install dirmngr" for the following
+# keys from https://www.python.org/downloads/#pubkeys
+KEYLIST_PYTHON_DEV="531F072D39700991925FED0C0EDDC5F26A45C816 26DEA9D4613391EF3E25C9FF0A5B101836580288 CBC547978A3964D14B9AB36A6AF053F07D9DC8D2 C01E1CAD5EA2C4F0B8E3571504C367C218ADD4FF 12EF3DC38047DA382D18A5B999CDEA9DA4135B38 8417157EDBE73D9EAC1E539B126EB563A74B06BF DBBF2EEBF925FAADCF1F3FFFD9866941EA5BBD71 2BA0DB82515BBB9EFFAC71C5C9BE28DEE6DF025C 0D96DF4D4110E5C43FBFB17F2D347EA6AA65421D C9B104B3DD3AA72D7CCB1066FB9921286F5E1540 97FC712E4C024BBEA48A61ED3A5CA953F73C700D 7ED10B6531D7C8E1BC296021FC624643487034E5"
+KEYRING_PYTHON_DEV="keyring-electrum-build-python-dev.gpg"
+for server in $(shuf -e ha.pool.sks-keyservers.net \
+                        hkp://p80.pool.sks-keyservers.net:80 \
+                        keyserver.ubuntu.com \
+                        hkp://keyserver.ubuntu.com:80) ; do
+    retry gpg --no-default-keyring --keyring $KEYRING_PYTHON_DEV --keyserver "$server" --recv-keys $KEYLIST_PYTHON_DEV \
+    && break || : ;
+done
+for msifile in core dev exe lib pip tools; do
+    echo "Installing $msifile..."
+    wget -N -c "https://www.python.org/ftp/python/$PYTHON_VERSION/win32/${msifile}.msi"
+    wget -N -c "https://www.python.org/ftp/python/$PYTHON_VERSION/win32/${msifile}.msi.asc"
+    verify_signature "${msifile}.msi.asc" $KEYRING_PYTHON_DEV
+    wine msiexec /i "${msifile}.msi" /qb TARGETDIR=$PYHOME
+done
+
+# Install dependencies specific to binaries
+# note that this also installs pinned versions of both pip and setuptools
+$PYTHON -m pip install -r "$here"/../deterministic-build/requirements-binaries.txt
+
+# Install PyInstaller
+$PYTHON -m pip install pyinstaller==3.4 --no-use-pep517
+
+# Install ZBar
+download_if_not_exist $ZBAR_FILENAME "$ZBAR_URL"
+verify_hash $ZBAR_FILENAME "$ZBAR_SHA256"
+wine "$PWD/$ZBAR_FILENAME" /S
+
+# Install NSIS installer
+download_if_not_exist $NSIS_FILENAME "$NSIS_URL"
+verify_hash $NSIS_FILENAME "$NSIS_SHA256"
+wine "$PWD/$NSIS_FILENAME" /S
+
+download_if_not_exist $LIBUSB_FILENAME "$LIBUSB_URL"
+verify_hash $LIBUSB_FILENAME "$LIBUSB_SHA256"
+7z x -olibusb $LIBUSB_FILENAME -aoa
+
+cp libusb/MS32/dll/libusb-1.0.dll $WINEPREFIX/drive_c/$PYTHON_FOLDER/
+
+mkdir -p $WINEPREFIX/drive_c/tmp
+cp secp256k1/libsecp256k1.dll $WINEPREFIX/drive_c/tmp/
+
+echo "Wine is configured."
---
+++ electrum/storage.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+#
+# Electrum - lightweight Bitcoin client
+# Copyright (C) 2015 Thomas Voegtlin
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import os
+import threading
+import stat
+import hashlib
+import base64
+import zlib
+
+from . import ecc
+from .util import PrintError, profiler, InvalidPassword, WalletFileException, bfh, standardize_path
+from .plugin import run_hook, plugin_loaders
+
+from .sqlite_db import SqliteDB
+from .json_db import JsonDB
+
+
+def get_derivation_used_for_hw_device_encryption():
+    return ("m"
+            "/4541509'"      # ascii 'ELE'  as decimal ("BIP43 purpose")
+            "/1112098098'")  # ascii 'BIE2' as decimal
+
+# storage encryption version
+STO_EV_PLAINTEXT, STO_EV_USER_PW, STO_EV_XPUB_PW = range(0, 3)
+
+
+
+class WalletStorage(PrintError):
+
+    def __init__(self, path, *, manual_upgrades=False, use_sql=False):
+        self.lock = threading.RLock()
+        self.path = standardize_path(path)
+        self._file_exists = self.path and os.path.exists(self.path)
+        DB_Class = SqliteDB# if use_sql else JsonDB
+        self.print_error("wallet path", self.path)
+        self.pubkey = None
+        if self.file_exists():
+            with open(self.path, "rb") as f:
+                self.raw = f.read()
+            self._encryption_version = self._init_encryption_version()
+            if not self.is_encrypted():
+                self.db = DB_Class(self.raw, manual_upgrades=manual_upgrades)
+                self.load_plugins()
+        else:
+            self._encryption_version = STO_EV_PLAINTEXT
+            # avoid new wallets getting 'upgraded'
+            self.db = DB_Class(b'', manual_upgrades=False)
+
+
+    def load_plugins(self):
+        wallet_type = self.db.get('wallet_type')
+        if wallet_type in plugin_loaders:
+            plugin_loaders[wallet_type]()
+
+    def put(self, key,value):
+        self.db.put(key, value)
+
+    def get(self, key, default=None):
+        return self.db.get(key, default)
+
+    @profiler
+    def write(self):
+        with self.lock:
+            self._write()
+
+    def _write(self):
+        if threading.currentThread().isDaemon():
+            self.print_error('warning: daemon thread cannot write db')
+            return
+        if not self.db.modified():
+            return
+
+        s = self.encrypt_before_writing(self.db.dump())
+        temp_path = "%s.tmp.%s" % (self.path, os.getpid())
+        with open(temp_path, "wb") as f:
+            f.write(s)
+            f.flush()
+            os.fsync(f.fileno())
+
+        mode = os.stat(self.path).st_mode if self.file_exists() else stat.S_IREAD | stat.S_IWRITE
+        if not self.file_exists():
+            assert not os.path.exists(self.path)
+        os.replace(temp_path, self.path)
+        os.chmod(self.path, mode)
+        self._file_exists = True
+        self.print_error("saved", self.path)
+        self.db.set_modified(False)
+
+    def file_exists(self):
+        return self._file_exists
+
+    def is_past_initial_decryption(self):
+        """Return if storage is in a usable state for normal operations.
+
+        The value is True exactly
+            if encryption is disabled completely (self.is_encrypted() == False),
+            or if encryption is enabled but the contents have already been decrypted.
+        """
+        try:
+            return bool(self.db.data)
+        except AttributeError:
+            return False
+
+    def is_encrypted(self):
+        """Return if storage encryption is currently enabled."""
+        return self.get_encryption_version() != STO_EV_PLAINTEXT
+
+    def is_encrypted_with_user_pw(self):
+        return self.get_encryption_version() == STO_EV_USER_PW
+
+    def is_encrypted_with_hw_device(self):
+        return self.get_encryption_version() == STO_EV_XPUB_PW
+
+    def get_encryption_version(self):
+        """Return the version of encryption used for this storage.
+
+        0: plaintext / no encryption
+
+        ECIES, private key derived from a password,
+        1: password is provided by user
+        2: password is derived from an xpub; used with hw wallets
+        """
+        return self._encryption_version
+
+    def _init_encryption_version(self):
+        try:
+            magic = base64.b64decode(self.raw)[0:4]
+            if magic == b'BIE1':
+                return STO_EV_USER_PW
+            elif magic == b'BIE2':
+                return STO_EV_XPUB_PW
+            else:
+                return STO_EV_PLAINTEXT
+        except:
+            return STO_EV_PLAINTEXT
+
+    @staticmethod
+    def get_eckey_from_password(password):
+        secret = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), b'', iterations=1024)
+        ec_key = ecc.ECPrivkey.from_arbitrary_size_secret(secret)
+        return ec_key
+
+    def _get_encryption_magic(self):
+        v = self._encryption_version
+        if v == STO_EV_USER_PW:
+            return b'BIE1'
+        elif v == STO_EV_XPUB_PW:
+            return b'BIE2'
+        else:
+            raise WalletFileException('no encryption magic for version: %s' % v)
+
+    def decrypt(self, password):
+        ec_key = self.get_eckey_from_password(password)
+        if self.raw:
+            enc_magic = self._get_encryption_magic()
+            s = zlib.decompress(ec_key.decrypt_message(self.raw, enc_magic))
+        else:
+            s = None
+        self.pubkey = ec_key.get_public_key_hex()
+        s = s.decode('utf8')
+        self.db = JsonDB(s, manual_upgrades=True)
+        self.load_plugins()
+
+    def encrypt_before_writing(self, plaintext: str) -> str:
+        s = plaintext
+        if self.pubkey:
+            s = bytes(s, 'utf8')
+            c = zlib.compress(s)
+            enc_magic = self._get_encryption_magic()
+            public_key = ecc.ECPubkey(bfh(self.pubkey))
+            s = public_key.encrypt_message(c, enc_magic)
+            s = s.decode('utf8')
+        return s
+
+    def check_password(self, password):
+        """Raises an InvalidPassword exception on invalid password"""
+        if not self.is_encrypted():
+            return
+        if self.pubkey and self.pubkey != self.get_eckey_from_password(password).get_public_key_hex():
+            raise InvalidPassword()
+
+    def set_keystore_encryption(self, enable):
+        self.put('use_encryption', enable)
+
+    def set_password(self, password, enc_version=None):
+        """Set a password to be used for encrypting this storage."""
+        if enc_version is None:
+            enc_version = self._encryption_version
+        if password and enc_version != STO_EV_PLAINTEXT:
+            ec_key = self.get_eckey_from_password(password)
+            self.pubkey = ec_key.get_public_key_hex()
+            self._encryption_version = enc_version
+        else:
+            self.pubkey = None
+            self._encryption_version = STO_EV_PLAINTEXT
+        # make sure next storage.write() saves changes
+        self.db.set_modified(True)
+
+    def requires_upgrade(self):
+        #if not self.is_past_initial_decryption():
+        #    raise Exception("storage not yet decrypted!")
+        return self.db.requires_upgrade()
+
+    def upgrade(self):
+        self.db.upgrade()
+        self.write()
+
+    def requires_split(self):
+        return self.db.requires_split()
+
+    def split_accounts(self):
+        out = []
+        result = self.db.split_accounts()
+        for data in result:
+            path = self.path + '.' + data['suffix']
+            storage = WalletStorage(path)
+            storage.db.data = data
+            storage.db.upgrade()
+            storage.write()
+            out.append(path)
+        return out
+
+    def get_action(self):
+        action = run_hook('get_action', self)
+        return action
